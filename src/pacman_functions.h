0e0ffadf663956434fc5ae3226ecd41b49d719a6
CayoMe <cayomendes@gmail.com>
2025-11-21 19:05:21 -0300

    Mudança no Mapa

--- src/pacman_functions.h
+++ src/pacman_functions.h
@@ -1,10 +1,11 @@
 #ifndef pacman_functions_h
 #define pacman_functions_h
-#define ROWS 7
-#define COLS 9
+#define ROWS 23
+#define COLS 22
 #include "pacman_structs.h"
 
 char getInput();
+void telaInicial();
 void gerarFantasmapa(char[ROWS][COLS], char[ROWS][COLS]);
 void renderGrid(char[ROWS][COLS], char[ROWS][COLS], Player, Fantasma, int *pontos);
 void moverJogador(Player*, int rows, int cols, char[ROWS][COLS], int *pontos);
