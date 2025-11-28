c72b28d89657e677f20131bcc257f25c474b5ab0
CayoMe <cayomendes@gmail.com>
2025-11-28 02:24:24 -0300

    Save + Reset

--- src/pacman_functions.h
+++ src/pacman_functions.h
@@ -2,15 +2,22 @@
 #define pacman_functions_h
 #define ROWS 23
 #define COLS 22
+#define pacman_inicial_x 9
+#define pacman_inicial_y 15
+#define fantasma_inicial_x 9
+#define fantasma_inicial_y 9
+
 #include "pacman_structs.h"
 
 char getInput();
 void telaInicial();
 void gerarFantasmapa(char[ROWS][COLS], char[ROWS][COLS]);
-void renderGrid(char[ROWS][COLS], char[ROWS][COLS], Player, Fantasma[], int, int *pontos);
-char moverJogador(Player*, int rows, int cols, char[ROWS][COLS], char[ROWS][COLS], int *pontos);
+void renderGrid(char[ROWS][COLS], char[ROWS][COLS], Player*, Fantasma[], int, int *pontos);
+char moverJogador(Player*, Fantasma[], int rows, int cols, char[ROWS][COLS], char[ROWS][COLS], int *pontos);
 void moverFantasma(Fantasma*, char[ROWS][COLS], Player);
 int mudarDirecao(Fantasma*, char[ROWS][COLS], int, int);
-void saveGame(Player, Fantasma[]);
+void saveGame(Player p, Fantasma f[]);
+void reiniciarJogo(Player *p, Fantasma f[], char mapa[ROWS][COLS]);
+void carregarMapa(char mapa[ROWS][COLS]);
 
 #endif /* pacman_functions_h */
